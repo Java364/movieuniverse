8201d02ea7a546558405db235e8d6ba2432f2287
Igor Flyunt <igflyunt@gmail.com>
2018-12-10 17:46:28 +0200

    add entity

---
+++ src/main/java/academy/softserve/movieuniverse/entity/UserReview.java
@@ -0,0 +1,57 @@
+package academy.softserve.movieuniverse.entity;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "user_review")
+public class UserReview extends AbstractEntity {
+    @ManyToOne(fetch = FetchType.LAZY)
+    private User reviewer;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    private Movie reviewedMovie;
+
+    @Column(columnDefinition = "text")
+    private String text;
+
+    @OneToMany(mappedBy = "userReview", cascade = CascadeType.ALL)
+    private List<CommentMark> commentMarks = new ArrayList<>();
+
+    public User getReviewer() {
+        return reviewer;
+    }
+
+    public UserReview setReviewer(User reviewer) {
+        this.reviewer = reviewer;
+        return this;
+    }
+
+    public Movie getReviewedMovie() {
+        return reviewedMovie;
+    }
+
+    public UserReview setReviewedMovie(Movie reviewedMovie) {
+        this.reviewedMovie = reviewedMovie;
+        return this;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public UserReview setText(String text) {
+        this.text = text;
+        return this;
+    }
+
+    public List<CommentMark> getCommentMarks() {
+        return commentMarks;
+    }
+
+    public UserReview setCommentMarks(List<CommentMark> commentMarks) {
+        this.commentMarks = commentMarks;
+        return this;
+    }
+}
