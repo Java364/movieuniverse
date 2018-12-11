7cd911e4f98e2da9418b8cd4dbf0b24cc0da1e9e
KatyaKaratnyk <k.karatnik@meta.ua>
2018-12-11 20:11:31 +0200

    Gallery add

---
+++ src/main/java/academy/softserve/movieuniverse/entity/Gallery.java
@@ -0,0 +1,28 @@
+package academy.softserve.movieuniverse.entity;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.persistence.Entity;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "gallery")
+public class Gallery extends AbstractEntity{
+	
+	@OneToMany
+	private List<Image> images = new ArrayList<>();
+
+	public Gallery() {}
+	public List<Image> getImages() {
+		return images;
+	}
+
+	public void setImages(List<Image> images) {
+		this.images = images;
+	}
+	
+	
+
+}
--- src/main/java/academy/softserve/movieuniverse/repository/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package academy.softserve.movieuniverse.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import academy.softserve.movieuniverse.entity.User;
-
-public interface UserRepository extends JpaRepository<User, Long>{
-
-}
