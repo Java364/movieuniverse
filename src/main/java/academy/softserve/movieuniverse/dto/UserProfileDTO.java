85f4944e0d6f24fab7ec5b07a0f3da0ead7105e5
Tania Kliuieva <tania.kliuieva@gmail.com>
2018-12-12 15:06:41 +0200

    create Service and dao

--- src/main/java/academy/softserve/movieuniverse/dto/UserProfileDTO.java
+++ src/main/java/academy/softserve/movieuniverse/dto/UserProfileDTO.java
@@ -1,6 +1,7 @@
 package academy.softserve.movieuniverse.dto;
 
 import java.time.LocalDate;
+import java.util.Objects;
 
 public class UserProfileDTO {
     private String email;
@@ -32,12 +33,37 @@ public class UserProfileDTO {
         this.firstName = firstName;
     }
 
+    public long getBirthday() {
+        return birthday;
+    }
+
+    public void setBirthday(long birthday) {
+        this.birthday = birthday;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        UserProfileDTO that = (UserProfileDTO) o;
+        return birthday == that.birthday &&
+                Objects.equals(email, that.email) &&
+                Objects.equals(lastName, that.lastName) &&
+                Objects.equals(firstName, that.firstName);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(email, lastName, firstName, birthday);
+    }
+
     @Override
     public String toString() {
         return "UserProfileDTO{" +
                 "email='" + email + '\'' +
                 ", lastName='" + lastName + '\'' +
                 ", firstName='" + firstName + '\'' +
+                ", birthday=" + birthday +
                 '}';
     }
 }
--- src/main/java/academy/softserve/movieuniverse/service/UserService.java
+++ src/main/java/academy/softserve/movieuniverse/service/UserService.java
@@ -1,5 +1,36 @@
 package academy.softserve.movieuniverse.service;
 
+import academy.softserve.movieuniverse.dto.UserProfileDTO;
+import academy.softserve.movieuniverse.dto.UserRegistrationDTO;
+import academy.softserve.movieuniverse.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public class UserService {
 
+    private final UserRepository userRepository;
+
+    @Autowired
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    private UserProfileDTO getUser(Long id){
+        userRepository.getOne(id);
+        return null;
+    }
+
+    private UserProfileDTO createUser(UserRegistrationDTO userDTO){
+        return null;
+    }
+
+    private void deleteUser(Long id){
+    }
+
+    private List<UserProfileDTO> getAll(){
+        return null;
+    }
 }
