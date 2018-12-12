8540b5ae5e0aec95c359aca996bafc2a2094c27f
Igor Flyunt <igflyunt@gmail.com>
2018-12-12 23:16:29 +0200

    enhance ReversableDtoMapper with method that converts entity list to dto list

--- src/main/java/academy/softserve/movieuniverse/service/mapper/ReversableDtoMapper.java
+++ src/main/java/academy/softserve/movieuniverse/service/mapper/ReversableDtoMapper.java
@@ -1,7 +1,14 @@
 package academy.softserve.movieuniverse.service.mapper;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 public interface ReversableDtoMapper<E, D> {
     E mapToEntity(D dto);
 
     D mapToDto(E entity);
+
+    default List<D> mapEntitiesListToDto(List<E> entities) {
+        return entities.stream().map(this::mapToDto).collect(Collectors.toList());
+    }
 }
