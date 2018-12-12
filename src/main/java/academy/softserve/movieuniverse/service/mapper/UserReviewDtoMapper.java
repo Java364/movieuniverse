579733ac5bb2898c87cf32a92c159e5f37011382
Igor Flyunt <igflyunt@gmail.com>
2018-12-12 18:29:09 +0200

    add user review dto mapper

---
+++ src/main/java/academy/softserve/movieuniverse/service/mapper/UserReviewDtoMapper.java
@@ -0,0 +1,27 @@
+package academy.softserve.movieuniverse.service.mapper;
+
+import academy.softserve.movieuniverse.dto.UserReviewDto;
+import academy.softserve.movieuniverse.entity.UserReview;
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
+public class UserReviewDtoMapper implements ReversableDtoMapper<UserReview, UserReviewDto> {
+    private ModelMapper modelMapper;
+
+    @Autowired
+    public UserReviewDtoMapper(ModelMapper modelMapper) {
+        this.modelMapper = modelMapper;
+    }
+
+    @Override
+    public UserReview mapToEntity(UserReviewDto dto) {
+        return modelMapper.map(dto, UserReview.class);
+    }
+
+    @Override
+    public UserReviewDto mapToDto(UserReview entity) {
+        return modelMapper.map(entity, UserReviewDto.class);
+    }
+}
