f4f963527fee9c47860acfbbd8cc1078e8fe1d24
Olexandr <olexandr.maltsev@gmail.com>
2018-12-12 16:46:46 +0200

    Renaimes file

--- src/main/java/academy/softserve/movieuniverse/repository/StarProfession.java
+++
@@ -1,7 +0,0 @@
-package academy.softserve.movieuniverse.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface StarProfession extends JpaRepository<StarProfession, Long> {
-
-}
