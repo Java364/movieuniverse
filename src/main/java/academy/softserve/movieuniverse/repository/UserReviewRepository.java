381ebfaa9d614a01f12334ec9223c24cfd8efc80
Igor Flyunt <igflyunt@gmail.com>
2018-12-12 18:50:02 +0200

    fix non-existing property name

--- src/main/java/academy/softserve/movieuniverse/repository/UserReviewRepository.java
+++ src/main/java/academy/softserve/movieuniverse/repository/UserReviewRepository.java
@@ -8,6 +8,6 @@ import org.springframework.data.repository.query.Param;
 import java.util.List;
 
 public interface UserReviewRepository extends JpaRepository<UserReview, Long> {
-    @Query("select ur from UserReview ur inner join ur.reviewedMovie rm where rm.movie_id = :movieId")
+    @Query("select ur from UserReview ur inner join ur.reviewedMovie rm where rm.id = :movieId")
     List<UserReview> findAllUserReviewsByMovieId(@Param("movieId") Long movieId);
 }
