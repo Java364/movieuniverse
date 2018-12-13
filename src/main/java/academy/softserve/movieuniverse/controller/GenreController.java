b99fa9dac2c0876182405bf4e4ce952ea7993cb4
Igor Flyunt <igflyunt@gmail.com>
2018-12-13 17:24:15 +0200

    add @RequestMapper to a GenreController

--- src/main/java/academy/softserve/movieuniverse/controller/GenreController.java
+++ src/main/java/academy/softserve/movieuniverse/controller/GenreController.java
@@ -2,9 +2,11 @@ package academy.softserve.movieuniverse.controller;
 
 import academy.softserve.movieuniverse.service.GenreService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/genre")
 public class GenreController {
     private GenreService genreService;
 
