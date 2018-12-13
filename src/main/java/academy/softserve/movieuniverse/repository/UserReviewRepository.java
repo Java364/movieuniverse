ae5f6dc53586d6abbe11b7b3fab34618fb043a44
Igor Flyunt <igflyunt@gmail.com>
2018-12-13 17:10:59 +0200

    fix:remove unneccessary method from UserReviewRepository

--- src/main/java/academy/softserve/movieuniverse/repository/UserReviewRepository.java
+++ src/main/java/academy/softserve/movieuniverse/repository/UserReviewRepository.java
@@ -7,5 +7,4 @@ import org.springframework.data.repository.query.Param;
 import java.util.List;
 
 public interface UserReviewRepository extends JpaRepository<UserReview, Long> {
-    List<UserReview> findAllUserReviewsByMovieId(Long movieId);
 }
