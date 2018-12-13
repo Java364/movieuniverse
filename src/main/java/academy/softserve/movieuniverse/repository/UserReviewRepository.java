42b44ff80f503ea73a7119b3ee2e48e07512d388
Igor Flyunt <igflyunt@gmail.com>
2018-12-13 16:27:33 +0200

    remove unnecessary named query from UserReviewRepository

--- src/main/java/academy/softserve/movieuniverse/repository/UserReviewRepository.java
+++ src/main/java/academy/softserve/movieuniverse/repository/UserReviewRepository.java
@@ -2,12 +2,10 @@ package academy.softserve.movieuniverse.repository;
 
 import academy.softserve.movieuniverse.entity.UserReview;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
 public interface UserReviewRepository extends JpaRepository<UserReview, Long> {
-    @Query("select ur from UserReview ur inner join ur.reviewedMovie rm where rm.id = :movieId")
     List<UserReview> findAllUserReviewsByMovieId(@Param("movieId") Long movieId);
 }
