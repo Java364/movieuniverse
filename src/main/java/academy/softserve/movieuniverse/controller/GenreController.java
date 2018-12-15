e9ed1e38eb61e8d86b8e423d740fc18ac9b05114
Igor Flyunt <igflyunt@gmail.com>
2018-12-15 23:51:29 +0200

    Implement GenreController

--- src/main/java/academy/softserve/movieuniverse/controller/GenreController.java
+++ src/main/java/academy/softserve/movieuniverse/controller/GenreController.java
@@ -1,34 +1,42 @@
 package academy.softserve.movieuniverse.controller;
 
+import academy.softserve.movieuniverse.dto.genre.GenreCreateDto;
 import academy.softserve.movieuniverse.dto.genre.GenreDto;
-import academy.softserve.movieuniverse.dto.genre.GenreEditorDto;
-import academy.softserve.movieuniverse.dto.genre.GenreViewDto;
+import academy.softserve.movieuniverse.entity.Genre;
 import academy.softserve.movieuniverse.repository.GenreRepository;
 import academy.softserve.movieuniverse.service.GenreService;
 import academy.softserve.movieuniverse.service.mapper.GenreDtoMapper;
 import com.fasterxml.jackson.annotation.JsonIgnore;
-import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @RestController
 @RequestMapping("/genre")
 public class GenreController {
-    @Autowired
-    private GenreRepository genreRepository;
-
     private GenreService genreService;
+    private GenreDtoMapper genreDtoMapper;
 
     @Autowired
-    public GenreController(GenreService genreService) {
+    public GenreController(GenreService genreService, GenreDtoMapper genreDtoMapper) {
         this.genreService = genreService;
+        this.genreDtoMapper = genreDtoMapper;
     }
 
-    @GetMapping("/one")
-    public ResponseEntity<GenreDto> createGenre() {
-        @JsonIgnore;
-        return new ResponseEntity<>(new GenreDto(), HttpStatus.OK);
+    @GetMapping("/show/all")
+    public ResponseEntity<List<GenreDto>> showAllGenres() {
+        final List<Genre> allGenres = genreService.findAllGenres();
+        final List<GenreDto> genresResponse = genreDtoMapper.mapEntitiesToGenreDtoList(allGenres);
+        return ResponseEntity.status(HttpStatus.OK).body(genresResponse);
+    }
+
+    @PostMapping("/create")
+    public ResponseEntity<?> createGenre(@RequestBody GenreCreateDto genreCreateDto) {
+        final Genre newGenre = genreDtoMapper.mapGenreCreateDtoToEntity(genreCreateDto);
+        genreService.saveGenre(newGenre);
+        return ResponseEntity.status(HttpStatus.CREATED).build();
     }
 }
