e82d1d485640838ab69cd5f0e61a23b5c4b3f384
Igor Flyunt <igflyunt@gmail.com>
2018-12-15 23:50:58 +0200

    provide GenreCreateDto interface deserializer

--- src/main/java/academy/softserve/movieuniverse/dto/genre/GenreCreateDto.java
+++ src/main/java/academy/softserve/movieuniverse/dto/genre/GenreCreateDto.java
@@ -1,5 +1,8 @@
 package academy.softserve.movieuniverse.dto.genre;
 
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+
+@JsonDeserialize(as = GenreDto.class)
 public interface GenreCreateDto {
     String getGenreName();
 
