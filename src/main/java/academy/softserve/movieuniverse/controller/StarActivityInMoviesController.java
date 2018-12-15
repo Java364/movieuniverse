892691af14efb0ca996eb92a9597d41187478adc
Olexandr Maltsev <olexandr.maltsev@gmail.com>
2018-12-15 19:38:30 +0200

    Adds Controller and Mapper. NOT READY. Issue with deserialization.

---
+++ src/main/java/academy/softserve/movieuniverse/controller/StarActivityInMoviesController.java
@@ -0,0 +1,36 @@
+package academy.softserve.movieuniverse.controller;
+
+import academy.softserve.movieuniverse.dto.StarActivityInMoviesDTO;
+import academy.softserve.movieuniverse.entity.StarActivityInMovies;
+import academy.softserve.movieuniverse.service.StarActivityInMoviesService;
+import academy.softserve.movieuniverse.service.mapper.StarActivityInMoviesMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+public class StarActivityInMoviesController {
+
+    private StarActivityInMoviesService starActivityInMoviesService;
+    private StarActivityInMoviesMapper starActivityInMoviesMapper;
+
+    @Autowired
+    public StarActivityInMoviesController(StarActivityInMoviesService starActivityInMoviesService, StarActivityInMoviesMapper starActivityInMoviesMapper) {
+        this.starActivityInMoviesService = starActivityInMoviesService;
+        this.starActivityInMoviesMapper = starActivityInMoviesMapper;
+    }
+
+    @GetMapping("/starActivityInMovies")
+    public List<StarActivityInMoviesDTO> viewAll() {
+        List<StarActivityInMovies> allStarActivityInMovies = starActivityInMoviesService.findAllStarActivityInMovies();
+        List<StarActivityInMoviesDTO> allStarActivityInMoviesDTOs = new ArrayList<>();
+        for (StarActivityInMovies starActivityInMovies: allStarActivityInMovies) {
+            StarActivityInMoviesDTO starActivityInMoviesDTO = starActivityInMoviesMapper.mapToDto(starActivityInMovies);
+            allStarActivityInMoviesDTOs.add(starActivityInMoviesDTO);
+        }
+        return allStarActivityInMoviesDTOs;
+    }
+}
--- src/main/java/academy/softserve/movieuniverse/repository/StarActivityInMoviesRepository.java
+++ src/main/java/academy/softserve/movieuniverse/repository/StarActivityInMoviesRepository.java
@@ -2,7 +2,9 @@ package academy.softserve.movieuniverse.repository;
 
 import academy.softserve.movieuniverse.entity.StarActivityInMovies;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface StarActivityInMoviesRepository extends JpaRepository<StarActivityInMovies, Long> {
 
 }
