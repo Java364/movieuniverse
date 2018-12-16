3c5dfc41d5669e39641a11fc629b2f7bdcbc96bf
Vitalii Lototskyi <loto9308@gmail.com>
2018-12-16 21:24:52 +0200

    MovieMark Exception added

--- src/main/java/academy/softserve/movieuniverse/exception/MovieMarkException.java
+++ src/main/java/academy/softserve/movieuniverse/exception/MovieMarkException.java
@@ -1,6 +1,6 @@
 package academy.softserve.movieuniverse.exception;
 
-public class MovieMarkException extends CustomValidationException{
+public class MovieMarkException extends CustomValidationException {
 
 	private static final long serialVersionUID = -9023090121189027086L;
 
@@ -8,4 +8,19 @@ public class MovieMarkException extends CustomValidationException{
 		super(message, exceptionType, ex);
 	}
 
+	public static MovieMarkException createSaveException(String message, Exception ex) {
+		return new MovieMarkException(message, ExceptionType.SAVE, ex);
+	}
+
+	public static MovieMarkException createUpdateException(String message, Exception ex) {
+		return new MovieMarkException(message, ExceptionType.UPDATE, ex);
+	}
+
+	public static MovieMarkException createDeleteException(String message, Exception ex) {
+		return new MovieMarkException(message, ExceptionType.DELETE, ex);
+	}
+	
+	public static MovieMarkException createSelectException(String message, Exception ex) {
+		return new MovieMarkException(message, ExceptionType.SELECT, ex);
+	}
 }
