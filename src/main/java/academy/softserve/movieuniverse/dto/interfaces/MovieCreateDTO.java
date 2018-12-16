07dc75049336b6bc391087527a189a5642ad47f1
Marichka <lunishinm@gmail.com>
2018-12-16 17:46:45 +0200

    movie delete, update

--- src/main/java/academy/softserve/movieuniverse/dto/interfaces/MovieCreateDTO.java
+++ src/main/java/academy/softserve/movieuniverse/dto/interfaces/MovieCreateDTO.java
@@ -1,9 +1,12 @@
 package academy.softserve.movieuniverse.dto.interfaces;
 
 import academy.softserve.movieuniverse.dto.GenreDto;
+import academy.softserve.movieuniverse.dto.MovieDTO;
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
 
 import java.util.List;
 
+@JsonDeserialize(as = MovieDTO.class)
 public interface MovieCreateDTO extends  MovieInfoDTO {
     public List<GenreDto> getGenres();
     public void setGenres(List<GenreDto> genres);
--- src/main/java/academy/softserve/movieuniverse/service/MovieService.java
+++ src/main/java/academy/softserve/movieuniverse/service/MovieService.java
@@ -1,10 +1,12 @@
 package academy.softserve.movieuniverse.service;
 
 import academy.softserve.movieuniverse.entity.Movie;
+import academy.softserve.movieuniverse.exception.MovieException;
 import academy.softserve.movieuniverse.repository.MovieRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
@@ -14,16 +16,71 @@ public class MovieService {
     private MovieRepository movieRepository;
 
     public Movie saveMovie(Movie movie) {
-        movie = movieRepository.save(movie);
+        if (movie == null) {
+            throw MovieException.movieSaveException("Can't save null object", null);
+        }
+        try {
+            movie = movieRepository.save(movie);
+        } catch (Exception ex) {
+            throw MovieException.movieSaveException("Can't save movie object", ex);
+        }
         return movie;
     }
 
     public List<Movie> showAllMovies() {
-        return movieRepository.findAll();
+        List<Movie> result = new ArrayList<>();
+        try {
+            result.addAll(movieRepository.findAll());
+        } catch (Exception ex) {
+            throw MovieException.movieSelectException("Can't get movies", ex);
+        }
+        return result;
     }
 
     public Movie findMovieById(Long id) {
         Optional<Movie> movie = movieRepository.findById(id);
+        if (!movie.isPresent()) {
+            throw MovieException.movieSelectException("Movie with id:" + id + " not found", null);
+        }
         return movie.get();
     }
+
+    public Movie updateMovie(Movie movie, Long id) {
+        Movie exist = findMovieById(id);
+        try {
+            if (isExist(movie.getMovieName()))
+                exist.setMovieName(movie.getMovieName());
+            if (isExist(movie.getDescription()))
+                exist.setDescription(movie.getDescription());
+            if (isExist(movie.getAgeLimitation()))
+                exist.setAgeLimitation(movie.getAgeLimitation());
+            if (movie.getDuration() != null && movie.getDuration() > 0)
+                exist.setDuration(movie.getDuration());
+            if (movie.getYear() > 1888)
+                exist.setYear(movie.getYear());
+            if (movie.getGenres() !=null && !movie.getGenres().isEmpty()) {
+                exist.setGenres(movie.getGenres());
+            }
+            exist = movieRepository.save(exist);
+        } catch (Exception ex) {
+            throw MovieException.movieUpdateException("Can't update movie", ex);
+        }
+        return  exist;
+    }
+
+    public void deleteMovie(Long id) {
+        try {
+            Movie movie = findMovieById(id);
+            movieRepository.delete(movie);
+        } catch (Exception ex) {
+            throw MovieException.movieDeleteException("Can't delete movie", ex);
+        }
+    }
+
+    private boolean isExist(String value) {
+        if (value == null)
+            return false;
+        return  !value.trim().isEmpty();
+    }
+
 }
