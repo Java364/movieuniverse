8b697725a4d72fc6d68c5197b684d0ce3ffdc884
Igor Flyunt <igflyunt@gmail.com>
2018-12-16 21:07:44 +0200

    remove methods with query annotation

--- src/main/java/academy/softserve/movieuniverse/repository/GenreRepository.java
+++ src/main/java/academy/softserve/movieuniverse/repository/GenreRepository.java
@@ -2,12 +2,7 @@ package academy.softserve.movieuniverse.repository;
 
 import academy.softserve.movieuniverse.entity.Genre;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
-
-import java.util.Optional;
 
 public interface GenreRepository extends JpaRepository<Genre, Long> {
-    @Query("select g from Genre g where g.name = :genreName")
-    Optional<Genre> findGenreByName(@Param("genreName") String genreName);
+
 }
