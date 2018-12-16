aad22cd9c5277a38564f8939e7a0fd76f16525c1
Igor Flyunt <igflyunt@gmail.com>
2018-12-16 21:27:20 +0200

    remove unnecessary interfaces from GenreDto class

--- src/main/java/academy/softserve/movieuniverse/dto/genre/GenreDto.java
+++ src/main/java/academy/softserve/movieuniverse/dto/genre/GenreDto.java
@@ -3,15 +3,13 @@ package academy.softserve.movieuniverse.dto.genre;
 import org.springframework.hateoas.core.Relation;
 
 @Relation(value = "genre", collectionRelation = "genres")
-public class GenreDto implements GenreCreateDto {
+public class GenreDto {
     private String genreName;
 
-    @Override
     public String getGenreName() {
         return genreName;
     }
 
-    @Override
     public void setGenreName(String genreName) {
         this.genreName = genreName;
     }
