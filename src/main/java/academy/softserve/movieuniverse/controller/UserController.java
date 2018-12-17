28b99f8dd2c3b31a9c2b8fc6a3211e2258126576
Tania Kliuieva <tania.kliuieva@gmail.com>
2018-12-17 15:14:46 +0200

    fixings

--- src/main/java/academy/softserve/movieuniverse/controller/UserController.java
+++ src/main/java/academy/softserve/movieuniverse/controller/UserController.java
@@ -1,20 +1,18 @@
 package academy.softserve.movieuniverse.controller;
 
-import academy.softserve.movieuniverse.controller.hateoas.UserResourceAssembler;
-import academy.softserve.movieuniverse.dto.user.UserDTO;
-import academy.softserve.movieuniverse.dto.user.UserShortInfoWithPassword;
+import academy.softserve.movieuniverse.controller.hateoas.userAssembler.UserShortInfoResourceAssembler;
+import academy.softserve.movieuniverse.dto.user.UserShortInfo;
 import academy.softserve.movieuniverse.service.UserService;
 import academy.softserve.movieuniverse.service.mapper.UserMapper;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.hateoas.Resource;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-import java.net.URI;
-import java.net.URISyntaxException;
 import java.util.List;
-import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping(value = "/api/users", produces = "application/hal+json")
@@ -22,59 +20,59 @@ public class UserController {
 
     private final UserService userService;
     private final UserMapper userMapper;
-    private final UserResourceAssembler userResourceAssembler;
+    private final UserShortInfoResourceAssembler userShortInfoResourceAssembler;
 
     @Autowired
-    public UserController(UserService userService, UserMapper userMapper, UserResourceAssembler userResourceAssembler) {
+    public UserController(UserService userService, UserMapper userMapper, UserShortInfoResourceAssembler userShortInfoResourceAssembler) {
         this.userService = userService;
         this.userMapper = userMapper;
-        this.userResourceAssembler = userResourceAssembler;
+        this.userShortInfoResourceAssembler = userShortInfoResourceAssembler;
     }
 
     @GetMapping
     public ResponseEntity<?> getAllUsers() {
-        List<UserDTO> users = userMapper.mapUserEntityListToUserWithShortInfoList(
-                userService.getAll()).stream().map(userShortInfo -> (UserDTO) userShortInfo).collect(Collectors.toList());
+        List<UserShortInfo> users = userMapper.mapUserEntityListToUserWithShortInfoList(
+                userService.getAll());
         return ResponseEntity
                 .status(HttpStatus.OK)
-                .body(userResourceAssembler.listToResource(users));
+                .body(userShortInfoResourceAssembler.listToResource(users));
     }
 
-    @PostMapping
-    public ResponseEntity<?> createUser(@RequestBody UserShortInfoWithPassword userDTO) throws URISyntaxException {
-        Resource<UserDTO> resource = userResourceAssembler.toResource((UserDTO)
-                userMapper.mapUserEntityToUserDTOWithShortInfo(
-                        userService.createUser(
-                                userMapper.mapUserShortInfoWithPasswordToEntity(userDTO)
-                        )
-                )
-        );
-        return ResponseEntity
-                .created(new URI(resource.getId().expand().getHref()))
-                .body(resource);
-    }
-
+//    @PostMapping
+//    public ResponseEntity<?> createUser(@RequestBody UserShortInfoWithPassword userDTO) throws URISyntaxException {
+//        Resource<UserDTO> resource = userResourceAssembler.toResource((UserDTO)
+//                userMapper.mapUserEntityToUserDTOWithShortInfo(
+//                        userService.createUser(
+//                                userMapper.mapUserShortInfoWithPasswordToEntity(userDTO)
+//                        )
+//                )
+//        );
+//        return ResponseEntity
+//                .created(new URI(resource.getId().expand().getHref()))
+//                .body(resource);
+//    }
+//
     @GetMapping("/{id}")
     public ResponseEntity getUser(@PathVariable Long id) {
         return new ResponseEntity(HttpStatus.OK);
     }
-
-    @PutMapping("/{id}")
-    public ResponseEntity updateUser(@PathVariable Long id, @RequestBody UserDTO userDTO) {
-        return new ResponseEntity(HttpStatus.OK);
-    }
-
-    @PatchMapping("/{id}")
-    public ResponseEntity markUserAsDelete(@PathVariable Long id) {
-        userService.markUserAsDelete(id);
-        return new ResponseEntity(HttpStatus.OK);
-    }
-
-    @DeleteMapping("/{id}")
-    public ResponseEntity completelyDeleteUser(@PathVariable Long id) {
-        userService.completelyDeleteUser(id);
-        return new ResponseEntity(HttpStatus.OK);
-    }
+//
+//    @PutMapping("/{id}")
+//    public ResponseEntity updateUser(@PathVariable Long id, @RequestBody UserDTO userDTO) {
+//        return new ResponseEntity(HttpStatus.OK);
+//    }
+//
+//    @PatchMapping("/{id}")
+//    public ResponseEntity markUserAsDelete(@PathVariable Long id) {
+//        userService.markUserAsDelete(id);
+//        return new ResponseEntity(HttpStatus.OK);
+//    }
+//
+//    @DeleteMapping("/{id}")
+//    public ResponseEntity completelyDeleteUser(@PathVariable Long id) {
+//        userService.completelyDeleteUser(id);
+//        return new ResponseEntity(HttpStatus.OK);
+//    }
 
 
 }
