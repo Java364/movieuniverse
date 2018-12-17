0463f5cc8dd07a527c26650ee1ecce9583bd88c5
Maksym-Paselsky <43058251+Maksym-Paselsky@users.noreply.github.com>
2018-12-17 02:05:52 +0200

    add all for userReviewMark and some changes to poster

--- src/main/java/academy/softserve/movieuniverse/controller/PosterController.java
+++ src/main/java/academy/softserve/movieuniverse/controller/PosterController.java
@@ -3,6 +3,7 @@ package academy.softserve.movieuniverse.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -32,7 +33,7 @@ public class PosterController {
 		return new ResponseEntity<PosterDTO>(posterDTO, HttpStatus.CREATED);
 	}
 
-	@PutMapping("/{id}")
+	@PutMapping("/update/{id}")
 	ResponseEntity<PosterDTO> updatePoster(@PathVariable("id") Long id, @RequestBody PosterDTO posterDTO) {
 		Poster poster = posterMapper.mapToEntityForUpdate(posterDTO, id);
 		poster = posterService.updatePoster(poster);
@@ -40,10 +41,15 @@ public class PosterController {
 		return new ResponseEntity<PosterDTO>(posterDTO, HttpStatus.OK);
 	}
 
-	@GetMapping("/{id}")
+	@GetMapping("/show/{id}")
 	public ResponseEntity<PosterDTO> showPoster(@PathVariable Long id) {
 		Poster poster = posterService.findPosterById(id);
 		return new ResponseEntity<>(posterMapper.mapToDto(poster), HttpStatus.OK);
+	}
+	@DeleteMapping("/delete/{id}")
+	public ResponseEntity<String> fullyDeletePoster(@PathVariable Long id) {
+		posterService.deletePoster(id);
+		return new ResponseEntity<String>(HttpStatus.OK);
 	}
 
 }
--- src/main/java/academy/softserve/movieuniverse/repository/UserReviewMark.java
+++
@@ -1,7 +0,0 @@
-package academy.softserve.movieuniverse.repository;
-
-import academy.softserve.movieuniverse.entity.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserReviewMark extends JpaRepository<User, Long> {
-}
