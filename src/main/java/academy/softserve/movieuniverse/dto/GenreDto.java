c2c6c5a54907625f605f71462cdf9da353fa6184
Igor Flyunt <igflyunt@gmail.com>
2018-12-17 22:32:10 +0200

    make GenreDTO class extend ResourceSupport class (i.e. make GenreDTO to b a hateoas resource)

--- src/main/java/academy/softserve/movieuniverse/dto/GenreDto.java
+++ src/main/java/academy/softserve/movieuniverse/dto/GenreDto.java
@@ -1,15 +1,29 @@
 package academy.softserve.movieuniverse.dto;
 
+import org.springframework.hateoas.ResourceSupport;
 import org.springframework.hateoas.core.Relation;
 
 @Relation(value = "genre", collectionRelation = "genres")
-public class GenreDto {
+public class GenreDto extends ResourceSupport implements GenreCreateUpdateRequest {
+    private Long genreId;
     private String genreName;
 
+    public Long getGenreId() {
+        return genreId;
+    }
+
+    public void setGenreId(Long genreId) {
+        this.genreId = genreId;
+    }
+
+
+    @Override
     public String getGenreName() {
         return genreName;
     }
 
+
+    @Override
     public void setGenreName(String genreName) {
         this.genreName = genreName;
     }
