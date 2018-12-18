22ce18a6dbd31d9f34d968d0743c138ec494cca4
Igor Flyunt <igflyunt@gmail.com>
2018-12-18 13:40:47 +0200

    implement UserReviewDto class

--- src/main/java/academy/softserve/movieuniverse/dto/UserReviewDto.java
+++ src/main/java/academy/softserve/movieuniverse/dto/UserReviewDto.java
@@ -1,4 +1,60 @@
 package academy.softserve.movieuniverse.dto;
 
-public class UserReviewDto {
+import org.springframework.hateoas.ResourceSupport;
+
+public class UserReviewDto extends ResourceSupport {
+    private Long userReviewId;
+    private String text;
+    private String userName;
+    private String movieName;
+    private Long creationTime;
+    private Long lastUpdated;
+
+    public Long getUserReviewId() {
+        return userReviewId;
+    }
+
+    public void setUserReviewId(Long userReviewId) {
+        this.userReviewId = userReviewId;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getMovieName() {
+        return movieName;
+    }
+
+    public void setMovieName(String movieName) {
+        this.movieName = movieName;
+    }
+
+    public Long getCreationTime() {
+        return creationTime;
+    }
+
+    public void setCreationTime(Long creationTime) {
+        this.creationTime = creationTime;
+    }
+
+    public Long getLastUpdated() {
+        return lastUpdated;
+    }
+
+    public void setLastUpdated(Long lastUpdated) {
+        this.lastUpdated = lastUpdated;
+    }
 }
