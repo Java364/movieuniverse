250eaf80af888a0eab7cce2e4ac3e85360aed3da
Igor Flyunt <igflyunt@gmail.com>
2018-12-18 15:24:40 +0200

    add JsonDeserializer for UsersReviewMovieView interface

--- src/main/java/academy/softserve/movieuniverse/dto/userreview/UsersReviewMovieView.java
+++ src/main/java/academy/softserve/movieuniverse/dto/userreview/UsersReviewMovieView.java
@@ -1,5 +1,8 @@
 package academy.softserve.movieuniverse.dto.userreview;
 
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+
+@JsonDeserialize(as = UserReviewDTO.class)
 public interface UsersReviewMovieView {
     Long getUserReviewId();
 
