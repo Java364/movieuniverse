049d6c73fb5f2fcf9576eb441888eacc16c53acb
Tania Kliuieva <tania.kliuieva@gmail.com>
2018-12-19 13:32:44 +0200

    fixings

--- src/main/java/academy/softserve/movieuniverse/exception/CountryException.java
+++ src/main/java/academy/softserve/movieuniverse/exception/CountryException.java
@@ -1,29 +1,29 @@
 package academy.softserve.movieuniverse.exception;
 
 
-public class CountryException extends CustomValidationException{
+public class CountryException extends CustomValidationException {
 
-	private static final long serialVersionUID = -6283677404347365061L;
+    private static final long serialVersionUID = -6283677404347365061L;
 
-	private CountryException(String customMessage) {
-		super(customMessage);
-	}
-	
-	public static CountryException createSaveException(String message) {
-		return new CountryException(message);
-	}
-	
-	public static CountryException createDeleteException(String message) {
-		return new CountryException(message);
-	}
-	
-	public static CountryException createUpdateException(String message) {
-		return new CountryException(message);
-	}
-	
-	public static CountryException createSelectException(String message) {
-		return new CountryException(message);
-	}
+    private CountryException(String customMessage) {
+        super(customMessage);
+    }
+
+    public static CountryException createSaveException(String message) {
+        return new CountryException(message);
+    }
+
+    public static CountryException createDeleteException(String message) {
+        return new CountryException(message);
+    }
+
+    public static CountryException createUpdateException(String message) {
+        return new CountryException(message);
+    }
+
+    public static CountryException createSelectException(String message) {
+        return new CountryException(message);
+    }
 
 
 }
