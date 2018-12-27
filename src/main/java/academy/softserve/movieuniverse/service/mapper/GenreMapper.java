8edb83566d005e3d4713bc4265780f66feb8ba41
Igor Flyunt <igflyunt@gmail.com>
2018-12-27 22:59:16 +0200

    remove ModelMapper class from GenreMapper class

--- src/main/java/academy/softserve/movieuniverse/service/mapper/GenreMapper.java
+++ src/main/java/academy/softserve/movieuniverse/service/mapper/GenreMapper.java
@@ -3,27 +3,23 @@ package academy.softserve.movieuniverse.service.mapper;
 import academy.softserve.movieuniverse.dto.genre.GenreDTO;
 import academy.softserve.movieuniverse.dto.genre.GenreRequest;
 import academy.softserve.movieuniverse.entity.Genre;
-import org.modelmapper.ModelMapper;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
 public class GenreMapper implements DTOMapper<GenreDTO, GenreRequest,  Genre> {
-    private ModelMapper modelMapper;
-
-    @Autowired
-    public GenreMapper(ModelMapper modelMapper) {
-        this.modelMapper = modelMapper;
-    }
-
     @Override
     public GenreDTO mapToDTO(Genre genre) {
-        return modelMapper.map(genre, GenreDTO.class);
+        GenreDTO genreDTO = new GenreDTO();
+        genreDTO.setId(genre.getId());
+        genreDTO.setGenreName(genre.getName());
+        return genreDTO;
     }
 
     @Override
     public Genre mapToEntity(GenreRequest dto) {
-        return modelMapper.map(dto, Genre.class);
+        Genre genre = new Genre();
+        genre.setName(dto.getGenreName());
+        return genre;
     }
 
     public static Genre mapToEntitySelectedGenre(GenreDTO genreDTO) {
