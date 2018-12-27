886239995c4362bbcc77310ba67347c978b9858b
Igor Flyunt <igflyunt@gmail.com>
2018-12-27 19:27:38 +0200

    add method to GenreMapper class to map selected genre

--- src/main/java/academy/softserve/movieuniverse/service/mapper/GenreMapper.java
+++ src/main/java/academy/softserve/movieuniverse/service/mapper/GenreMapper.java
@@ -1,15 +1,11 @@
 package academy.softserve.movieuniverse.service.mapper;
 
-import academy.softserve.movieuniverse.controller.GenreController;
 import academy.softserve.movieuniverse.dto.genre.GenreDTO;
 import academy.softserve.movieuniverse.dto.genre.GenreRequest;
 import academy.softserve.movieuniverse.entity.Genre;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.hateoas.Link;
 import org.springframework.stereotype.Component;
-
-import static org.springframework.hateoas.mvc.ControllerLinkBuilder.linkTo;
 
 @Component
 public class GenreMapper implements DTOMapper<GenreDTO, GenreRequest,  Genre> {
@@ -22,14 +18,18 @@ public class GenreMapper implements DTOMapper<GenreDTO, GenreRequest,  Genre> {
 
     @Override
     public GenreDTO mapToDTO(Genre genre) {
-        GenreDTO genreDTO = modelMapper.map(genre, GenreDTO.class);
-        Link selfRelLink = linkTo(GenreController.class).slash(genre.getId()).withSelfRel();
-        genreDTO.add(selfRelLink);
-        return genreDTO;
+        return modelMapper.map(genre, GenreDTO.class);
     }
 
     @Override
     public Genre mapToEntity(GenreRequest dto) {
         return modelMapper.map(dto, Genre.class);
+    }
+
+    public static Genre mapToEntitySelectedGenre(GenreDTO genreDTO) {
+        Genre genre = new Genre();
+        genre.setId(genreDTO.getId());
+        genre.setName(genreDTO.getGenreName());
+        return genre;
     }
 }
