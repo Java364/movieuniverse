1d8e728b307e4eab7a11bfcf0a3d5c174f2b0a71
Solomiia <yaremkosolomiya@gmail.com>
2018-12-30 16:10:04 +0200

    the end

--- src/main/java/academy/softserve/movieuniverse/exception/NoSuchEntityException.java
+++ src/main/java/academy/softserve/movieuniverse/exception/NoSuchEntityException.java
@@ -1,8 +1,8 @@
 package academy.softserve.movieuniverse.exception;
 
 public class NoSuchEntityException extends CustomValidationException {
+
     public NoSuchEntityException(String message) {
         super(message);
     }
-
 }
