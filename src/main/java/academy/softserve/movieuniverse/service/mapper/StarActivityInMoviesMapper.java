ddfc3e584977f2e2910092a031095fa5d4dc83b9
Igor Flyunt <igflyunt@gmail.com>
2019-01-01 12:11:38 +0200

    StarActivityInMoviesMapper class hotfix

--- src/main/java/academy/softserve/movieuniverse/service/mapper/StarActivityInMoviesMapper.java
+++ src/main/java/academy/softserve/movieuniverse/service/mapper/StarActivityInMoviesMapper.java
@@ -2,12 +2,10 @@ package academy.softserve.movieuniverse.service.mapper;
 
 import academy.softserve.movieuniverse.dto.StarActivityInMoviesDTO;
 import academy.softserve.movieuniverse.entity.StarActivityInMovies;
-import academy.softserve.movieuniverse.entity.StarProfession;
 import org.springframework.stereotype.Component;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.stream.Collectors;
 
 @Component
 public class StarActivityInMoviesMapper {
@@ -16,15 +14,9 @@ public class StarActivityInMoviesMapper {
         starActivityInMoviesDTO.setId(starActivityInMoviesEntity.getId());
         starActivityInMoviesDTO.setMovieId(starActivityInMoviesEntity.getMovie().getId());
         starActivityInMoviesDTO.setMovieName(starActivityInMoviesEntity.getMovie().getMovieName());
-        starActivityInMoviesDTO.setStarId(starActivityInMoviesEntity.getStar().getId());
-        starActivityInMoviesDTO.setStarName(starActivityInMoviesEntity.getStar().getFirstName() + " "
-                + starActivityInMoviesEntity.getStar().getLastName());
+
 
         List<Long> ids = new ArrayList<>();
-        List<StarProfession> starProfessions = starActivityInMoviesEntity.getProfessions();
-        for (StarProfession profession : starProfessions) {
-            ids.add(profession.getId());
-        }
         return starActivityInMoviesDTO;
     }
 
@@ -40,8 +32,6 @@ public class StarActivityInMoviesMapper {
         StarActivityInMoviesDTO dto = new StarActivityInMoviesDTO();
         dto.setId(entity.getId());
         dto.setMovieId(entity.getMovie().getId());
-        dto.setStarId(entity.getStar().getId());
-        dto.setProfessionIds(entity.getProfessions().stream().map(p -> p.getId()).collect(Collectors.toList()));
         return dto;
     }
 }
