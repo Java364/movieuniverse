f7c40827bdfe28c470e8bd3d44bd9de0349d2b09
Vitalii Lototskyi <loto9308@gmail.com>
2019-01-02 19:36:36 +0200

    profession methods removed

--- src/main/java/academy/softserve/movieuniverse/repository/StarRepository.java
+++ src/main/java/academy/softserve/movieuniverse/repository/StarRepository.java
@@ -1,7 +1,6 @@
 package academy.softserve.movieuniverse.repository;
 
 import academy.softserve.movieuniverse.entity.Links;
-import academy.softserve.movieuniverse.entity.Profession;
 import academy.softserve.movieuniverse.entity.Star;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
@@ -11,7 +10,5 @@ import java.util.List;
 @Repository
 public interface StarRepository extends JpaRepository<Star, Long> {
     Star findByLinks(Links links);
-
     List<Star> findAllByFirstNameContainingOrLastNameContainingAllIgnoreCase(String firstName, String lastName);
-    List<Star> findAllByProfession(Profession profession);
 }
--- src/main/java/academy/softserve/movieuniverse/service/StarService.java
+++ src/main/java/academy/softserve/movieuniverse/service/StarService.java
@@ -2,7 +2,6 @@ package academy.softserve.movieuniverse.service;
 
 import academy.softserve.movieuniverse.entity.Gallery;
 import academy.softserve.movieuniverse.entity.Links;
-import academy.softserve.movieuniverse.entity.Profession;
 import academy.softserve.movieuniverse.entity.Star;
 import academy.softserve.movieuniverse.exception.ExceptionType;
 import academy.softserve.movieuniverse.exception.NotFoundException;
@@ -104,9 +103,5 @@ public class StarService {
 
     public List<Star> findAllByName(String name) {
         return starRepository.findAllByFirstNameContainingOrLastNameContainingAllIgnoreCase(name, name);
-    }
-    
-    public List<Star> findByProfession(Profession profession) {
-    	return starRepository.findAllByProfession(profession);
     }
 }
