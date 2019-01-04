e09fcb95910093f8f4dbb0abf207b17bb43b509f
Igor Flyunt <igflyunt@gmail.com>
2019-01-04 12:43:58 +0200

    add @JsonProperty annotation above CreditDTO field

--- src/main/java/academy/softserve/movieuniverse/dto/castcrew/CastDTO.java
+++ src/main/java/academy/softserve/movieuniverse/dto/castcrew/CastDTO.java
@@ -1,13 +1,18 @@
 package academy.softserve.movieuniverse.dto.castcrew;
 
-public class CastDTO extends Credit {
+import academy.softserve.movieuniverse.dto.star.CreditDTO;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+public class CastDTO {
+    @JsonProperty("star")
+    private CreditDTO creditDTO;
     private String character;
 
-	public String getCharacter() {
-		return character;
-	}
+    public String getCharacter() {
+        return character;
+    }
 
-	public void setCharacter(String character) {
-		this.character = character;
-	}
+    public void setCharacter(String character) {
+        this.character = character;
+    }
 }
