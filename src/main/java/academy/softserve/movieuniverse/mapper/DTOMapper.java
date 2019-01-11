f33d57dc0ccb8184570fc7c7e76c7e9ea8823bb0
Igor Flyunt <igflyunt@gmail.com>
2019-01-11 23:17:07 +0200

    optimize entity-dto mapping

--- src/main/java/academy/softserve/movieuniverse/mapper/DTOMapper.java
+++ src/main/java/academy/softserve/movieuniverse/mapper/DTOMapper.java
@@ -1,5 +1,6 @@
 package academy.softserve.movieuniverse.mapper;
 
+import java.util.Collection;
 import java.util.List;
 import java.util.Set;
 import java.util.function.Function;
@@ -10,20 +11,16 @@ public interface DTOMapper<D, I, E> {
 
     D mapToDTO(E entity);
 
-    default List<D> mapToDTOList(List<E> entities) {
+    default List<D> mapToDTOList(Collection<E> entities) {
         return entities.stream().map(this::mapToDTO).collect(Collectors.toList());
-    }
-
-    default List<D> mapToDTOList(Set<E> entities) {
-        return entities.stream().map(this::mapToDTO).collect(Collectors.toList());
-    }
-
-    default List<E> mapToEntityList(List<I> dtos) {
-        return dtos.stream().map(this::mapToEntity).collect(Collectors.toList());
     }
 
     default <T> List<T> mapToDTOList(List<E> entities, Function<E, T> mappingFunction) {
         return entities.stream().map(mappingFunction).collect(Collectors.toList());
+    }
+
+    default List<E> mapToEntityList(List<I> dtos) {
+        return dtos.stream().map(this::mapToEntity).collect(Collectors.toList());
     }
 
     default <T> List<E> mapToEntityList(List<T> dtos, Function<T, E> mappingFunction) {
@@ -33,6 +30,4 @@ public interface DTOMapper<D, I, E> {
     default <T> Set<E> mapToEntitySet(List<T> entities, Function<T, E> mappingFunction) {
         return entities.stream().map(mappingFunction).collect(Collectors.toSet());
     }
-
-
 }
