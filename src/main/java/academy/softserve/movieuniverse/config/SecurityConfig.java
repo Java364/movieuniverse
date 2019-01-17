05933229bce1249c59df455264b0010098f79135
Solomiia <yaremkosolomiya@gmail.com>
2019-01-17 14:30:51 +0200

    new

--- src/main/java/academy/softserve/movieuniverse/config/SecurityConfig.java
+++ src/main/java/academy/softserve/movieuniverse/config/SecurityConfig.java
@@ -5,6 +5,7 @@ import academy.softserve.movieuniverse.security.JwtAuthFilter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
